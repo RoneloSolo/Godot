91b1df34c2e2d99cb071d893cf562b49d7b4d494
DuckRaGod <73057950+DuckRaGod@users.noreply.github.com>
2022-10-22 21:42:40 +0300

    Add files via upload

--- Clan/Scr/BuildManager.cs
+++ Clan/Scr/BuildManager.cs
@@ -3,7 +3,7 @@ using System;
 
 public class BuildManager : Node2D{
 
-	int tileSize = 16;
+	int tileSize = 8;
 	Vector2 mousePos;
 
 	[Export] public BuildPlan[] buildPlans;
@@ -17,14 +17,17 @@ public class BuildManager : Node2D{
 
 	public override void _Process(float delta){
 		mousePos = GetGlobalMousePosition();
-		if(building != null)
+		if(building != null){
 			building.Position = new Vector2(Mathf.Stepify(mousePos.x,tileSize), Mathf.Stepify(mousePos.y,tileSize));
+			if(Input.IsActionPressed("Build")){
+				buildingLayer.AddChild(building);
+				building = null;
+			}
+		}
 	}
 
-	private void House0Button(){
-		building = (Node2D)buildPlans[0].GetPrefab().Instance();
+	private void House0Button(int i){
+		building = (Node2D)buildPlans[i].GetPrefab().Instance();
 		buildingLayer.AddChild(building);
 	}
 }
-
-
